51806469239761f2963311e078ef276b03e28d82
authentic-pipe-8414 <tasher.stidham@outlook.com>
2020-12-06 16:14:46 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,4 +10,10 @@ def countdown(num)
 end
 
 def countdown_with_sleep(num)
-  while 
+  while num > 0
+    puts "#{num} SECOND(S)!"
+    num -= 1 
+    sleep(2)
+  end 
+  return "HAPPY NEW YEAR!" 
+end
