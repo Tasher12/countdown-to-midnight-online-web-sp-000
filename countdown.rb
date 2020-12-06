90d8463d17139116c668dcec9071877b24f3ec00
authentic-pipe-8414 <tasher.stidham@outlook.com>
2020-12-06 16:08:27 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,10 @@
 #write your code here
 
-num = 12  
+num = 10  
 def countdown(num)
   while num > 0
     puts "#{num} SECOND(S)!"
     num -= 1 
-    break 
   return "HAPPY NEW YEAR!" 
   end
 end
